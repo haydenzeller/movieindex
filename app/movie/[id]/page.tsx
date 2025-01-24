c94800bcaf70c312fec443552188377c9081d546
Hayden Zeller <hayden@haydenzeller.com>
2025-01-24 18:10:05 -0500

    init

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -38,6 +38,10 @@ export default function Movie({ params }: { params: Promise<{ id: string }> }) {
             }
         })();
     }, [id]);
+    // const url = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent"
+    if (!movie.id) {
+        return <div></div>;
+    }
     return (
         <div className="flex flex-col items-center w-full h-screen">
             <div className="">
@@ -45,28 +49,38 @@ export default function Movie({ params }: { params: Promise<{ id: string }> }) {
                     <Image
                         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
                         alt={movie.title + " backdrop"}
-                        className=" w-full object-fill h-auto"
+                        className=" w-full object-fill h-auto blur z-1"
                         width={1920}
                         height={1080}
                         priority
                     />
                 </div>
-                <div className="">
+                {/* <div className="">
                     <Image
                         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                         alt={movie.title + " poster"}
                         width={300}
                         height={500}
-                        className="rounded-lg w-[20%] h-auto absolute top-[20%] left-10"
+                        className="rounded-lg w-[20%] h-auto  top-[20%] left-10"
                         priority
                     />
-                </div>
+                </div> */}
             </div>
-            <div className="flex flex-col items-start w-full pl-[25%]">
-                <h1 className="text-3xl font-bold">{movie.title}</h1>
-                <p className="text-lg">{movie.tagline}</p>
-                <p className="text-lg">{movie.overview}</p>
-                <p className="text-lg">{movie.budget}</p>
+            <div className="flex flex-row items-start w-full gap-2 p-10">
+                <Image
+                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                    alt={movie.title + " poster"}
+                    width={300}
+                    height={500}
+                    className="rounded-lg z-10 w-auto h-auto mt-[-200]"
+                    priority
+                />
+                <div className="flex p-6 flex-col items-start w-full">
+                    <h1 className="text-4xl font-bold">{movie.title}</h1>
+                    <p className="text-3xl font-bold">{movie.tagline}</p>
+                    <br />
+                    <p className="text-2xl">{movie.overview}</p>
+                </div>
             </div>
         </div>
     );
