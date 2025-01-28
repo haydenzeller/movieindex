f5cac5aa7cec02a9ff9333f285bda38177941752
Hayden Zeller <hayden@haydenzeller.com>
2025-01-27 20:18:38 -0500

    style changes

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -39,41 +39,48 @@ export default function Movie({ params }: { params: Promise<{ id: string }> }) {
             }
         })();
     }, [id]);
-    if (!movie.id) {
-        return <div></div>;
-    }
     return (
-        <div className="flex flex-col items-center w-full h-auto">
-            <div className="">
-                <div className="z-1 w-full h-80 items-center justify-center flex flex-col overflow-hidden">
+        <div className="relative min-w-screen min-h-screen">
+            <div
+                className="absolute inset-0"
+                style={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundAttachment: "fixed",
+                    filter: "blur(8px)",
+                }}
+            />
+            <div
+                className="absolute inset-0"
+                style={{
+                    background:
+                        "linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.95))",
+                }}
+            />
+            <div className="relative flex flex-col items-center w-full min-h-screen z-10">
+                <div className="flex lg:flex-row flex-col items-center lg:items-start w-full p-3">
                     <Image
-                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                        alt={movie.title + " backdrop"}
-                        className=" w-full object-fill h-auto blur z-1"
-                        width={1920}
-                        height={1080}
+                        src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                        alt={movie.title + " poster"}
+                        width={300}
+                        height={500}
+                        className="rounded-lg z-10 w-auto h-auto md:mx-5"
                         priority
                     />
+                    <div className="flex flex-col items-center lg:items-start h-auto w-full">
+                        <h1 className="text-4xl font-bold">{movie.title}</h1>
+                        <p className="text-3xl font-bold italic">
+                            {movie.tagline}
+                        </p>
+                        <br />
+                        <p className="text-2xl">{movie.overview}</p>
+                        <div className="w-full lg:w-1/2 md:w-3/4">
+                            <Video movie={movie} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-row items-start w-full gap-2 p-10">
-                <Image
-                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                    alt={movie.title + " poster"}
-                    width={300}
-                    height={500}
-                    className="rounded-lg z-10 w-auto h-auto mt-[-200]"
-                    priority
-                />
-                <div className="flex p-6 flex-col items-start w-full">
-                    <h1 className="text-4xl font-bold">{movie.title}</h1>
-                    <p className="text-3xl font-bold">{movie.tagline}</p>
-                    <br />
-                    <p className="text-2xl">{movie.overview}</p>
-                </div>
-            </div>
-            <div className="p-20 flex flex-col items-center justify-center w-auto">
-                <Video movie={movie} />
             </div>
         </div>
     );
