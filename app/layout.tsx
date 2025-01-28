c22ded177f90d04d78bd9418fb8a18679721451a
Hayden Zeller <hayden@haydenzeller.com>
2025-01-27 21:43:52 -0500

    Speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Funnel_Display } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./Providers";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const funDisplay = Funnel_Display({
     variable: "--font-funnel-display",
@@ -31,6 +32,7 @@ export default function RootLayout({
     return (
         <html lang="en" className="dark">
             <Analytics />
+            <SpeedInsights />
             <body className={funDisplay.variable}>
                 <Providers>{children}</Providers>
             </body>
