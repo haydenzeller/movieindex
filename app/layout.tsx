0c07bbeebf4459bbe424a04e6d50c817f7acbcad
Hayden Zeller <hayden@haydenzeller.com>
2025-01-27 21:43:22 -0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Funnel_Display } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./Providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const funDisplay = Funnel_Display({
     variable: "--font-funnel-display",
@@ -12,8 +13,14 @@ export const metadata: Metadata = {
     title: "Open Cinemas",
     description: "Free movies for everyone | Open Cinemas ",
     viewport: "width=device-width, initial-scale=1",
-    keywords: ["movies", "free", "open", "cinemas", "open cinemas", "movies free"],
-
+    keywords: [
+        "movies",
+        "free",
+        "open",
+        "cinemas",
+        "open cinemas",
+        "movies free",
+    ],
 };
 
 export default function RootLayout({
@@ -23,6 +30,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="dark">
+            <Analytics />
             <body className={funDisplay.variable}>
                 <Providers>{children}</Providers>
             </body>
